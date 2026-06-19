ab2c4165da6d7d354c28d68de00137662e720a89
rickykhulal <withrickytalk@gmail.com>
2026-06-19 14:11:13 +0545

    Add files via upload

---
+++ src/interactions/buttons/interactions_buttons_createkey.js
@@ -0,0 +1,15 @@
+// src/interactions/buttons/createkey.js
+import keyApproveHandler, {
+    keyDenyHandler,
+    keyPoolYesHandler,
+    keyPoolNoHandler,
+    keyPoolConfirmHandler,
+} from '../../handlers/createkeyButtons.js';
+
+export default [
+    keyApproveHandler,
+    keyDenyHandler,
+    keyPoolYesHandler,
+    keyPoolNoHandler,
+    keyPoolConfirmHandler,
+];
