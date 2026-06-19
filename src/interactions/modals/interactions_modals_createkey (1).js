bdb5af17ce980ab5c7d1d44e3e1b4af261d3e39e
rickykhulal <withrickytalk@gmail.com>
2026-06-19 14:12:07 +0545

    Add files via upload

---
+++ src/interactions/modals/interactions_modals_createkey (1).js
@@ -0,0 +1,8 @@
+// src/interactions/modals/createkey.js
+import createKeyModalHandler from '../../handlers/createkeyModal.js';
+import keyPoolDaysModalHandler from '../../handlers/createkeyPoolModal.js';
+
+export default [
+    createKeyModalHandler,
+    keyPoolDaysModalHandler,
+];
