0c57aa95a9d5901d2e845672a4f63bbd039e808f
rickykhulal <withrickytalk@gmail.com>
2026-06-20 20:15:55 +0545

    Delete src/interactions/modals/createkey.js

--- src/interactions/modals/createkey.js
+++
@@ -1,8 +0,0 @@
-// src/interactions/modals/createkey.js
-import createKeyModalHandler from '../../handlers/createkeyModal.js';
-import keyPoolDaysModalHandler from '../../handlers/createkeyPoolModal.js';
-
-export default [
-    createKeyModalHandler,
-    keyPoolDaysModalHandler,
-];
