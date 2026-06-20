752407eb860cf0e38b9e4ea59625e229b4995ed1
rickykhulal <withrickytalk@gmail.com>
2026-06-20 21:19:13 +0545

    Update commands.js

--- src/interactions/buttons/commands.js
+++ src/interactions/buttons/commands.js
@@ -1,5 +1,4 @@
-import commandsPrevHandler, { commandsNextHandler } from '../../handlers/commandsButtons.js';
-
+import { commandsPrevHandler, commandsNextHandler } from '../../handlers/commandsButtons.js';
 
 export default [
     commandsPrevHandler,
